c48285f10c60389dc1dac54814c15d361c0f9dc6
s32274 <s32274@pjwstk.edu.pl>
2025-03-25 13:57:00 +0100

    Prawie wszystkie metody w CargoShip

--- APBD_03/CargoShip.cs
+++ APBD_03/CargoShip.cs
@@ -2,5 +2,47 @@ namespace APBD_03;
 
 public class CargoShip
 {
+    private List<Container> containers { get; }
+    private static int maxId = 0;
+    private int id;
+    private double maxSpeed;
+    private int maxContainersCount;
+    private double maxContainersMass;
+
+    public CargoShip()
+    {
+        id = maxId;
+        maxId++;
+    }
+
+    public void LoadContainerOnShip(Container container)
+    {
+        this.containers.Add(container);
+    }
+
+    public void LoadContainersOnShip(List<Container> containers)
+    {
+        foreach (Container con in containers)
+        {
+            this.containers.Add(con);
+        }
+    }
+
+    public void removeContainer(Container container)
+    {
+        this.containers.Remove(container);
+    }
+
+    public void swapContainer(string id, Container container)
+    {
+        foreach (Container c in containers)
+        {
+            if (c.GetSerialId().Equals(id))
+            {
+                containers.Insert(containers.IndexOf(c), container);
+            }
+        }
+    }
+    
     
 }
